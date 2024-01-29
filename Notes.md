7aad9787b9d722111501b148812af60dd04ca379
Joey Huang <joeyh6547@gmail.com>
2024-01-29 10:11:59 -0500

    .

---
+++ Notes.md
@@ -0,0 +1,5 @@
+Q: What is an Iterable?
+
+ADT - abstract data type
+
+
---
+++ src/ClassFiles/InClassExercise/IntCollection.java
@@ -0,0 +1,39 @@
+package ClassFiles.InClassExercise;
+
+import java.util.Arrays;
+
+public class IntCollection {
+   private int[] a;
+
+   public IntCollection(int[] keys) {
+      a = Arrays.copyOf(keys, keys.length); // defensive copy
+      Arrays.sort(a);
+   }
+
+   public boolean contains(int key) { return indexOf(key) != -1; }
+
+   private int indexOf(int key) {
+      // indexOf() implemented as a linear search.
+      int l = 0;
+      int r = a.length - 1;
+      int mid;
+      while (l <= r){
+         mid = l + (r - l) / 2;
+         if (a[mid] == key){
+            return 0;
+         }
+         else if (a[mid] < key){
+            l = mid + 1;
+         }
+         else{
+            r = mid - 1;
+         }
+      }
+      //for (int i : a){
+      //   if (i == key){
+      //      return 0;
+      //   }
+      //}
+      return -1;
+   }
+}
