c3151036c07c769a644514e21b3ff03e973622e5
Joey Huang <joeyh6547@gmail.com>
2024-02-07 13:36:13 -0500

    .

--- src/ClassFiles/InsertPlus.java
+++ src/ClassFiles/InsertPlus.java
@@ -5,5 +5,11 @@ public class InsertPlus {
         ResizingArrayList<String> list = new ResizingArrayList<>();
         list.add("one"); list.add("two"); list.add("three"); list.add("four");
 
+        System.out.println(list);
+        //insert "+"
+        for (int i = 1; i < list.size(); i += 2){
+            list.add(i,"+");
+        }
+        System.out.println(list);
     } 
 }
