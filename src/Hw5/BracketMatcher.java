2fb5e184187f8410d789484b00f7d4cc43e68001
Joey Huang <joeyh6547@gmail.com>
2024-02-22 10:13:16 -0500

    hw5

---
+++ src/Hw5/BracketMatcher.java
@@ -0,0 +1,33 @@
+package Hw5;
+import java.util.Stack;
+
+public class BracketMatcher {
+
+   public static void main(String[] args) {
+      In in = new In("./src/Hw5/test06.txt");
+      String text = in.readAll();
+      System.out.println("processing: " + text); // comment out or delete this line before submitting
+      Stack<Character> openingBrackets = new Stack<>();
+      for (int position = 1; position <= text.length(); ++position) {
+         char next = text.charAt(position - 1);
+
+         /* YOUR CODE HERE */
+         if (next == '[' || next == '(' || next == '{'){
+            openingBrackets.push(next);
+         } else if (next == ']' && openingBrackets.pop() != '['){
+            System.out.println(position);
+            System.exit(0);
+         } else if (next == ')' && openingBrackets.pop() != '('){
+            System.out.println(position);
+            System.exit(0);
+         }  else if (next == '}' && openingBrackets.pop() != '{'){
+            System.out.println(position);
+            System.exit(0);
+         }
+      }
+      // Print "Success" if brackets are balanced
+      /* YOUR CODE HERE */
+      System.out.println("Success");
+   }
+}
+
